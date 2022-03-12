4babf1260d2d3183f6e2efc941eee034e0bca2ce
TharinduDeshananda <tdeshananda@gmail.com>
2022-03-12 23:04:05 +0530

    added styles for app

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,20 @@
 import { Typography, CssBaseline, AppBar, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
-
-const App = () => {
+import useStyles from './style';
+const App = () => { 
+    let classes = useStyles();
     return <>
         <CssBaseline />
         <AppBar position='relative'>
             <Toolbar>
-                <PhotoCamera />
-                <Typography variant='h3'>Photo Album</Typography>
+                <PhotoCamera className={classes.photoIcon}/>
+                <Typography variant='h4'>Photo Album</Typography>
             </Toolbar>
         </AppBar>
         
     <main>
         <div>
-            <Container maxWidth="sm">
+            <Container maxWidth="sm" className={classes.mainTitle}>
                 <Typography variant='h2' color='textPrimary' gutterBottom align='center'>Photo Album</Typography>
                 <Typography variant='h5' align='center'>This is a test project for material ui learn</Typography>
             </Container>
---
+++ src/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme)=>({
+mainTitle:{
+    marginTop:'100px'
+},
+photoIcon:{
+    fontSize:'84px',
+    marginRight:"20px"
+}
+}));
+
+export default useStyles;

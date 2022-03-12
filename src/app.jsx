f81e20f04aaca662321704b861058ac98401e185
TharinduDeshananda <tdeshananda@gmail.com>
2022-03-12 20:54:08 +0530

    header and titles added

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,25 @@
-const App = ()=>{
-    return <div>Hello Tharindu</div>;
+import { Typography, CssBaseline, AppBar, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+const App = () => {
+    return <>
+        <CssBaseline />
+        <AppBar position='relative'>
+            <Toolbar>
+                <PhotoCamera />
+                <Typography variant='h3'>Photo Album</Typography>
+            </Toolbar>
+        </AppBar>
+        
+    <main>
+        <div>
+            <Container maxWidth="sm">
+                <Typography variant='h2' color='textPrimary' gutterBottom align='center'>Photo Album</Typography>
+                <Typography variant='h5' align='center'>This is a test project for material ui learn</Typography>
+            </Container>
+        </div>
+    </main>
+    </>
 };
 
 export default App;
